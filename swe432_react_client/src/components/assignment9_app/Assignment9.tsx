ed23c96966c8fadf0908da44c8a5a35c92ce6542
Deval Parikh <devalpp@gmail.com>
2020-11-12 09:41:30 -0500

    modularized styling

--- swe432_react_client/src/components/assignment9_app/Assignment9.tsx
+++ swe432_react_client/src/components/assignment9_app/Assignment9.tsx
@@ -1,59 +1,52 @@
 import * as React from 'react'
-import {useState} from 'react';
+import './Assignment9.css'
+import { useState } from 'react';
 import { StringDisplay } from './StringDisplay';
 import { StringItem } from './StringItem';
-import {StringCreate } from './StringCreate';
-
-const initialState = { count: 0 };
-
-type ACTIONTYPE =
-  | { type: "increment"; payload: number }
-  | { type: "decrement"; payload: string };
-
-
+import { StringCreate } from './StringCreate';
 
 export function Assignment9() {
-  const [storedStrings, setStoredStrings] = useState([<StringItem str=""/>]);
+  const [storedStrings, setStoredStrings] = useState([<StringItem str="" />]);
   const [writtenStrings, setWrittenStrings] = useState([""]);
   const [currStr, setCurrStr] = useState("");
   const [withReplacement, setWithReplacement] = useState(false);
   const [withoutReplacement, setWithoutReplacement] = useState(true);
 
-  function onSubmit(){
+  function onSubmit() {
 
+    // Get array of written strings
     let tempArr = [...writtenStrings].filter(str => str != "");
 
     let max = tempArr.length;
 
-    if(max == 0){
+    // No written strings
+    if (max == 0) {
       alert("Please enter strings in the left box.");
       return;
     }
-   
 
+    // Choose random string from written strings
     let index = Math.floor(Math.random() * Math.floor(max));
     let randStr = tempArr[index];
 
     setCurrStr(randStr); //setting recent string
-    setStoredStrings([...storedStrings, <StringItem str={randStr}/>]); //storing in array
+    setStoredStrings([...storedStrings, <StringItem str={randStr} />]); //storing in array
 
-    if(withoutReplacement){
+    if (withoutReplacement) {
       let newArr = tempArr;
-      newArr.splice(index, 1);
+      newArr.splice(index, 1); //remove CurrStr
       setWrittenStrings(newArr);
-    }else{
-      setWrittenStrings(tempArr);
     }
 
   }
 
-  function onReset(){
+  function onReset() {
     setWrittenStrings([]);
     setCurrStr("");
     setStoredStrings([]);
   }
 
-  function clearChoseStrings(){
+  function clearChoseStrings() {
     setCurrStr("");
     setStoredStrings([]);
   }
@@ -79,15 +72,15 @@ export function Assignment9() {
             </a>
         </h3>
       </div>
-      <div style={{width:"100vw", height:"550px", display:"flex", justifyContent:"center"}}>
-        <div style={{width:"1000px", height:"550px", display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-evenly", }}>
-          <StringCreate updateStrings={(list:string[]):void => {setWrittenStrings(list)}} writtenStrings={writtenStrings}/>
-          <div style={{width:"200px", height:"200px", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"space-evenly", }}>
-            <label> <input style={{color:"white"}} type="radio" checked={withReplacement} onClick={() => {setWithReplacement(true); setWithoutReplacement(false);}}/> With Replacement </label>
-            <label> <input style={{color:"white"}} type="radio" checked={withoutReplacement} onClick={() => {setWithReplacement(false); setWithoutReplacement(true);}}/> Without Replacement </label>
-            <button onClick={onSubmit} style={{color:"white",width:"100px", height:"25px", backgroundColor:"gray", border:"none", borderRadius:"25px"}}> Submit </button>
-            <button onClick={onReset} style={{color:"white",width:"100px", height:"25px", backgroundColor:"gray", border:"none", borderRadius:"25px"}}> Reset </button>
-            <button onClick={clearChoseStrings} style={{color:"white",width:"150px", height:"25px", backgroundColor:"gray", border:"none", borderRadius:"25px"}}> Clear Chosen Strings </button>
+      <div className="menu-container">
+        <div className="menu-subcontainer">
+          <StringCreate updateStrings={(list: string[]): void => { setWrittenStrings(list) }} writtenStrings={writtenStrings} />
+          <div style={{ width: "200px", height: "200px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly", }}>
+            <label> <input type="radio" checked={withReplacement} onClick={() => { setWithReplacement(true); setWithoutReplacement(false); }} /> With Replacement </label>
+            <label> <input type="radio" checked={withoutReplacement} onClick={() => { setWithReplacement(false); setWithoutReplacement(true); }} /> Without Replacement </label>
+            <button className="menu-button" onClick={onSubmit}> Submit </button>
+            <button className="menu-button" onClick={onReset}> Reset </button>
+            <button className="menu-button" onClick={clearChoseStrings} style={{ width: "150px" }}> Clear Chosen Strings </button>
           </div>
           <StringDisplay currentStrings={storedStrings} randomString={currStr} />
         </div>
--- swe432_react_client/src/components/assignment9_app/StringDisplay.tsx
+++ swe432_react_client/src/components/assignment9_app/StringDisplay.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-interface stringProps{
+interface stringProps {
     randomString?: string;
     currentStrings?: Array<any>;
 }
 
-export function StringDisplay(props: stringProps){
+export function StringDisplay(props: stringProps) {
 
-    return(
-        <div style={{ borderRadius:"25px",alignItems:"center",display:"flex", flexDirection:"column",width:"250px", height:"500px", color:"white", backgroundColor:"gray"}}>
-            <div style={{borderRadius:"25px",paddingTop:"10px", paddingBottom:"10px",backgroundColor:"inherit", width:"inherit", textAlign:"center", maxHeight:"40px"}}> String Chosen: {props.randomString == null ? "" : props.randomString} </div>
-            <div style={{overflowY:"auto", color:"white",height:"430px", width:"200px", backgroundColor:"#10d2e0",display:"flex", flexDirection:"column", alignItems:"center"}}> {props.currentStrings} </div>
+    return (
+        <div className="string-container">
+            <div className="string-container-title"> String Chosen: {props.randomString === null ? "" : props.randomString} </div>
+            <div className="string-subcontainer"> {props.currentStrings} </div>
         </div>
     );
 }
